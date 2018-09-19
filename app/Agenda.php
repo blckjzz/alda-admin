269c1d4ac2e00b696244a23e7730ecf0a1da85cb
Diego Cerqueira <xdiegocerqueira@gmail.com>
2018-09-19 17:29:24 -0300

    mutator for recovering the formated data added

--- app/Agenda.php
+++ app/Agenda.php
@@ -2,14 +2,18 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Agenda extends Model
 {
-    protected $table = 'agendas';
-
     protected $primaryKey = 'id';
 
+    protected $table = 'agendas';
+
+    protected $fillable = ['data, hora, endereco, conselho_id, status_id, data_hora'];
+
+    protected $dates = ['data'];
 
     public function conselho()
     {
@@ -20,4 +24,13 @@ class Agenda extends Model
     {
         return $this->hasMany('App\Resultado', 'agenda_id');
     }
+
+    public function getDataAttribute($data)
+    {
+        if ($data == null || $data == '')
+            return null;
+        else
+            return Carbon::parse($data)->format('d/m/Y');
+    }
+
 }
