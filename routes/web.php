9690471e160fb4b4e348499cd53346b3547ca004
Diego Cerqueira <xdiegocerqueira@gmail.com>
2019-02-08 17:32:17 -0200

    new routes added for conselheiro and remove unused ones

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,29 @@ Route::get('/', function () {
     return redirect('/admin');
 });
 
-Route::get('/db', function (){
-    try {
-        DB::connection()->getPdo();
-        return \App\Agenda::first();
-
-    } catch (\Exception $e) {
-        die("Could not connect to the database.  Please check your configuration. error:" . $e );
-    }
-});
+//Route::get('/db', function (){
+//    try {
+//        DB::connection()->getPdo();
+//        return \App\Agenda::first();
+//
+//    } catch (\Exception $e) {
+//        die("Could not connect to the database.  Please check your configuration. error:" . $e );
+//    }
+//});
 
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
+
+});
+
+//Route::group(['prefix' => 'admin'], function(){
+//    Route::resource('conselheiro', 'ConselheiroController');
+//})->middleware('checkConselheiro');
+
+Route::group([
+    'prefix' => 'admin',
+    'middleware' => ['checkConselheiro']
+], function() {
+    Route::Resource('conselheiro','ConselheiroController');
 });
