94c8032c8308ea007e0e8856bf3004d06d4980e4
Diego Cerqueira <xdiegocerqueira@gmail.com>
2019-02-11 16:44:04 -0200

    method to find dir by id

--- app/Http/Controllers/ConselheiroController.php
+++ app/Http/Controllers/ConselheiroController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\CheckConselheiro;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
@@ -18,18 +19,9 @@ class ConselheiroController extends Controller
     {
         $user = Auth::user();
         $agendas = $user->conselho->agendas;
-        return view('conselheiro.browser', compact('agendas'));
+        return view('conselheiro.pauta.browser', compact('agendas'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return $this->index();
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -42,48 +34,17 @@ class ConselheiroController extends Controller
         dd($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function viewCCS()
     {
-        //
+        $conselho = Auth::user()->conselho;
+        return view('conselheiro.conselho.index', compact ('conselho'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function editCCS(Request $request)
     {
-        //
+        dd($request);
+        return $conselho;
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
