f90e8b8e635af57ba31b3c2edbea3980cc74fd5f
Diego Cerqueira <xdiegocerqueira@gmail.com>
2019-02-11 16:42:41 -0200

    mutators for recover and set dates correcly

--- app/Diretoria.php
+++ app/Diretoria.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Diretoria extends Model
@@ -12,9 +13,43 @@ class Diretoria extends Model
 
     protected $primaryKey = 'id';
 
+    protected $fillable = ['inicio_gestao', 'fim_gestao','cargo', 'nome'];
+
 
     public function conselho()
     {
         return $this->hasOne('App\Conselho', 'conselho_id');
+    }
+
+    public function getInicioGestaoAttribute($data)
+    {
+        if ($data == null || $data == '')
+            return "00/00/0000";
+        else
+            $date = Carbon::parse($data)->format('d/m/Y');
+        return $date;
+
+    }
+
+    public function getFimGestaoAttribute($data)
+    {
+        if ($data == null || $data == '')
+            return "00/00/0000";
+        else
+            $date = Carbon::parse($data)->format('d/m/Y');
+        return $date;
+
+    }
+
+    public function setInicioDaGestaoAttribute($data)
+    {
+        $data = DateTime::createFromFormat('d/m/Y', $data);
+        $this->attributes['data'] = $data;
+    }
+
+    public function setFimGestaoAttribute($data)
+    {
+        $data = DateTime::createFromFormat('d/m/Y', $data);
+        $this->attributes['data'] = $data;
     }
 }
