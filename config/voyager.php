d42f90c3181e1b2030536e1e60d441c5ffaabc04
Diego Cerqueira <xdiegocerqueira@gmail.com>
2019-02-13 18:23:57 -0200

    new config and js lib added

--- config/voyager.php
+++ config/voyager.php
@@ -124,7 +124,7 @@ return [
         /*
          * Select default language
          */
-        'default' => 'en',
+        'default' => 'pt-BR',
 
         /*
          * Select languages that are supported.
@@ -208,6 +208,8 @@ return [
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
         //'css/custom.css',
+        'css/jquery.tagit.css',
+        'css/tagit.ui-zendesk.css'
     ],
 
     'additional_js' => [
