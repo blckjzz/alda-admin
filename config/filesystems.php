6460701ac6f271a43558b7c3998410e9289e95cd
Diego Cerqueira <xdiegocerqueira@gmail.com>
2019-08-29 12:01:22 -0300

    fix on default disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'public'),
+    'default' => env('FILESYSTEM_DRIVER', 'default'),
 
     /*
     |--------------------------------------------------------------------------
@@ -59,7 +59,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage2',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
